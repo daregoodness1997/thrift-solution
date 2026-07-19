b7ee7e3b620d574ffef2b5d6837bb78a15d07f9e
Dare Goodness <daregoodness@gmail.com>
2026-07-19 17:40:09 +0100

    fix next build

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@thrift/types", "@thrift/config", "@thrift/ui"],
+  webpack: (config) => {
+    config.resolve.conditionNames = [
+      "@thrift/source",
+      ...(config.resolve.conditionNames || ["require", "node", "default"]),
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
